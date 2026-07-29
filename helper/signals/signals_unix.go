85588744c186eae085094c152c4736ddf342d245
Shen Li <641124270@qq.com>
2026-07-29 10:00:14 +0800

    ci: satisfy copywrite header checks

--- helper/signals/signals_unix.go
+++ helper/signals/signals_unix.go
@@ -1,6 +1,6 @@
 //go:build linux || darwin || freebsd || openbsd || solaris || netbsd
 
-// Copyright IBM Corp. 2014, 2026
+// Copyright IBM Corp. 2015, 2026
 // SPDX-License-Identifier: MPL-2.0
 
 package signals
