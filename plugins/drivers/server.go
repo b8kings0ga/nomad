dc31af6f55ee7c4b289c89a38fafda8d1c815945
Shen Li <641124270@qq.com>
2026-08-23 01:19:18 +0800

    fix: propagate disabled log collection capability

--- plugins/drivers/server.go
+++ plugins/drivers/server.go
@@ -62,6 +62,7 @@ func (b *driverPluginServer) Capabilities(ctx context.Context, req *proto.Capabi
 			Exec:                  caps.Exec,
 			MustCreateNetwork:     caps.MustInitiateNetwork,
 			NetworkIsolationModes: []proto.NetworkIsolationSpec_NetworkIsolationMode{},
+			DisableLogCollection:  caps.DisableLogCollection,
 			DynamicWorkloadUsers:  caps.DynamicWorkloadUsers,
 		},
 	}
